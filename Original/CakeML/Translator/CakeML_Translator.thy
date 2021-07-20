46bc555ebb0f6a2686b3cb770ff631941e37484f
Jonas Raedle <jonas.raedle@tum.de>
2021-07-20 14:35:27 +0200

    CakeML for original Kernel

---
+++ Original/CakeML/Translator/CakeML_Translator.thy
@@ -0,0 +1,19 @@
+theory CakeML_Translator
+  imports "CakeML_Semantics_Original.CakeML_Semantics"
+begin
+
+declare [[ML_environment="HOL4"]]
+
+subsection \<open>translator\<close>
+ML \<open>Context_Var.bind_ref "CakeML_Translator"\<close>
+
+ML \<open>Holmake run (make_modules [
+  "ml_progScript",
+  "ml_translatorScript",
+  "ml_pmatchScript",
+  "ml_optimiseScript"
+] ) "../../cakeml/translator"\<close>
+
+ML\<open>Holmake run (make_modules ["std_preludeScript"]) "../../cakeml/translator"\<close> 
+
+end
